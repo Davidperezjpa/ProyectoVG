5ef0e18710b638b58c0678933878f5714d2db562
Francisco <a01634745@itesm.mx>
2019-02-22 12:28:02 -0600

    commit

---
+++ VGs/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(h *5* Time.deltaTime, 0, 0, Space.World);
+        float jump = Input.GetAxisRaw("Jump");
+        if(jump==1 && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+}

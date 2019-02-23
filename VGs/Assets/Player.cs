bb3cc9bd625de3c72b56333a6b1603486da8cb05
Carlos Lopez <a01633683@itesm.mx>
2019-02-23 15:18:14 -0600

    Charlie Commitments #1
    +Double Jump
    +Dash with cooldown
    +UI text of enemy drop

--- VGs/Assets/Player.cs
+++ VGs/Assets/Player.cs
@@ -1,35 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    //Jumping
     private bool isGrounded;
+    private int isJumping = 0;
+
+    //Dashing
+    private bool isDashing = false;
+    private Coroutine dashCoroutine;
+
+    //Enemy drop score
+    public Text textito;
+    private int enemyDrop;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        this.enemyDrop = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Enemy drop score
+        textito.text = "Soul: " + enemyDrop;
+
+        //Horizontal movement
         float h = Input.GetAxis("Horizontal");
         transform.Translate(h *5* Time.deltaTime, 0, 0, Space.World);
-        float jump = Input.GetAxisRaw("Jump");
-        if(jump==1 && isGrounded)
+
+        //Jumping movement
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping < 1)
         {
-            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
-            isGrounded = false;
+            rb.AddForce(new Vector2(0, 8), ForceMode2D.Impulse);
+            this.isGrounded = false;
+            this.isJumping++;
+            
         }
+
+       
+
+        //Dash movement
+        if (Input.GetKey(KeyCode.E) && isDashing==false)
+        {
+            this.isDashing = true;
+            if (h > 0) //Derecha
+            {
+                rb.AddForce(new Vector2(30, 0), ForceMode2D.Impulse);
+            }
+            else if (h < 0) // Izquierda
+            {
+                rb.AddForce(new Vector2(-30, 0), ForceMode2D.Impulse);
+            }
+
+            dashCoroutine = StartCoroutine(dashCooldown());
+
+        }
+
+        
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 8)
         {
-            isGrounded = true;
+            this.isGrounded = true;
+            this.isJumping = 0;
         }
+    }
+
+    IEnumerator dashCooldown()
+    {
+        yield return new WaitForSeconds(0.15f);
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(1f);
+        this.isDashing = false;
+        StopCoroutine(dashCooldown());
     }
 }

48dcdf5de64dee849477af98a1c62776e56375ca
Alan Gonzalez <argaalan@hotmail.com>
2019-02-26 08:50:38 -0600

    Hook feature added

--- VGs/Assets/Hook.cs
+++ VGs/Assets/Hook.cs
@@ -5,51 +5,80 @@ using UnityEngine;
 public class Hook : MonoBehaviour
 {
 
-    private DistanceJoint2D joint;
-    private Vector3 targetPos;
-    private RaycastHit2D hit;
-    public float distance = 10f;
-    public LayerMask mask;
+    private float distance;
     private Vector2 targetPosition;
-
+    private float lasthook,x,y;
+    private Rigidbody2D playerRb;
+    private LayerMask lm;
+    private bool hooking, canHook;
+    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
     {
-        joint = GetComponent<DistanceJoint2D>();
-        joint.enabled = false;
+        distance = 10f;
+        lasthook = 0;
+        playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+
+        lm = ~(1 << 12);
+        hooking = false;
+        canHook = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        float hook = Input.GetAxisRaw("Hook");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        if (hook == 1 && lasthook == 0 && (h!=0 || v!=0) && canHook)
         {
-
-            // for the use of controller where he is looking
-
-
-
-            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPos.z = 0;
-            // for the use of controller where he is looking:
-            // hit = Physics2D.Raycast(transform.position, new Vector2(transform.localScale.x, 1), distance, mask);
-
-            // for the use of Mouse:
-            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
-
-            if (hit.collider != null /*&&  hit.collider.gameObject.GetComponent<Rigidbody2D>() != null*/)
+            hit = Physics2D.Raycast(transform.position,new Vector2(h,v), distance,lm);
+            if (hit.collider != null)
             {
-                joint.enabled = true;
-                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-                //joint.distance = Vector2.Distance(transform.position, hit.point);  -> Works like a lazo
-                joint.distance = 1/*hit.collider.bounds.size.y*/;
+                canHook = false;
+                Player.canMove = false;
+                hooking = true;
+                x = hit.point.x - transform.position.x;
+                y = hit.point.y - transform.position.y;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    y = y / Mathf.Abs(x);
+                    if (x >= 0) x = 1; else x = -1;
+                }
+                else if (Mathf.Abs(y) > Mathf.Abs(x))
+                {
+                    x = x / Mathf.Abs(y);
+                    if (y >= 0) y = 1; else y = -1;
+                }
+                else
+                {
+                    x = y = 1;
+                }
+            }
+            else print(null);
+        }
+
+        if (hooking)
+        {
+            print(x + "......" + y);
+            playerRb.velocity = new Vector2(x*10,y*10);
+            if (hook == 0 && lasthook == 1)
+            {
+                Player.canMove = true;
+                playerRb.velocity = Vector2.up;
+                hooking = false;
             }
         }
-        if (Input.GetKeyUp(KeyCode.E))
+        lasthook = hook;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player.canMove = true;
+        hooking = false;
+        if (collision.gameObject.layer == 8)
         {
-            joint.enabled = false;
+            canHook = true;
         }
-
     }
 }

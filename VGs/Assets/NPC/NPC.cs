b3dd7ad6019dcf1f9b708eacf1d3daaf115a353a
David Perez <davidperezjpa@gmail.com>
2019-04-09 08:37:22 -0500

    Tienda (Incompleta) No funcionan botones
    asdf

--- VGs/Assets/NPC/NPC.cs
+++ VGs/Assets/NPC/NPC.cs
@@ -5,29 +5,57 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     public GameObject player;
+    public Dialogue dialogue;
+    private bool dialogueActive;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Inventory")==1)
+        if (Input.GetAxisRaw("Inventory") == 1)
         {
             double distance = Vector2.Distance(transform.position, player.transform.position);
             if (distance <= 5)
             {
+                dialogueActive = true;
+
+                if (dialogueActive == true)
+                {
+                    dialogueActive = false;
+                    TriggerDialogue();
+
+                }
+                
+
+                
+                  
+
+                
 
 
 
             }
+            else
+            {
+                //dialogueActive = false;
+            }
+            
 
 
         }
     }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
 }
---
+++ VGs/Assets/Tienda/Tienda.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tienda : MonoBehaviour
+{
+
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MejoraVida()
+    {
+
+        print("mejoraste la vida :D");
+        //player.SetLife();
+
+    }
+
+    public void MejoraEspada()
+    {
+        print("mejoraste la espada :D");
+        //player.SetSword();
+    }
+
+    public void MejoraSpeed()
+    {
+        print("mejoraste la speed :D");
+        //player.SetSpeed();
+    }
+
+}
